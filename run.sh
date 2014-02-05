31fb437b9c1a8ddf8d4df16084462e603ed49601
Cristian Mircea Messel <mess110@gmail.com>
2014-02-05 22:14:23 +0200

    add run script

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+source /home/kiki/.rvm/environments/ruby-2.1.0@coinmarketcap
+ruby ./script.rb
--- script.rb
+++ script.rb
@@ -5,6 +5,9 @@ require 'json'
 require 'nokogiri'
 require 'pp'
 
+current_folder = File.dirname(File.expand_path(__FILE__))
+path = File.join(current_folder, 'public', 'api')
+
 doc = Nokogiri::HTML(open("http://coinmarketcap.com/"))
 # File.open('coinmarketcap','w') {|f| doc.write_html_to f}
 # doc = Nokogiri::HTML(File.open('coinmarketcap', 'r'))
@@ -19,10 +22,10 @@ doc.css("#currencies tbody tr").each do |tr|
   coin[-1] = tr.attribute('id').text
   h = Hash[keys.zip(coin)]
   h['timestamp'] = Time.now.to_i
-  File.open("public/api/#{coin[-1]}.json",'w') { |f| f.write(h.to_json) }
+  File.open("#{path}/#{coin[-1]}.json",'w') { |f| f.write(h.to_json) }
   h.delete('timestamp')
   r << h
 end
 
 rr = { 'timestamp' => ts, 'markets' => r }
-File.open('public/api/all.json','w') {|f| f.write(rr.to_json) }
+File.open("#{path}/all.json",'w') {|f| f.write(rr.to_json) }
