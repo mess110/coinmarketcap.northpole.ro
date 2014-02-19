f1533fd6466cf1c0d3426799e51fc9dcba7ebade
Cristian Mircea Messel <mess110@gmail.com>
2014-02-19 21:04:13 +0200

    add public/api to cap shared dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/home/kiki/coinmarketcap.northpole.ro'
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{public/api}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
