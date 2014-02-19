26961fcb7649985a0444f4c7673942525daf3f7b
Cristian Mircea Messel <mess110@gmail.com>
2014-02-19 20:08:40 +0200

    get data as btc and usd

--- script.rb
+++ script.rb
@@ -6,26 +6,44 @@ require 'nokogiri'
 require 'pp'
 
 current_folder = File.dirname(File.expand_path(__FILE__))
-path = File.join(current_folder, 'public', 'api')
+@path = File.join(current_folder, 'public', 'api')
 
-doc = Nokogiri::HTML(open("http://coinmarketcap.com/"))
-# File.open('coinmarketcap','w') {|f| doc.write_html_to f}
-# doc = Nokogiri::HTML(File.open('coinmarketcap', 'r'))
+@doc = Nokogiri::HTML(open("http://coinmarketcap.com/"))
+# File.open('coinmarketcap','w') {|f| @doc.write_html_to f}
+# @doc = Nokogiri::HTML(File.open('coinmarketcap', 'r'))
 
-ts = Time.now.to_i
+@ts = Time.now.to_i
+
 # order is important
-keys = ['position', 'name', 'marketCap', 'price', 'totalSupply', 'volume24', 'change24', 'id']
+@keys = ['position', 'name', 'marketCap', 'price', 'totalSupply', 'volume24', 'change24', 'timestamp', 'currency', 'id']
 
-r = []
-doc.css("#currencies tbody tr").each do |tr|
-  coin = tr.css('td').collect{ |td| td.text.strip.gsub('*','') }
-  coin[-1] = tr.attribute('id').text
-  h = Hash[keys.zip(coin)]
-  h['timestamp'] = Time.now.to_i
-  File.open("#{path}/#{coin[-1]}.json",'w') { |f| f.write(h.to_json) }
-  h.delete('timestamp')
-  r << h
+def write_to_disk currency
+  r = []
+  @doc.css("#currencies tbody tr").each do |tr|
+    tds = tr.css('td')
+    coin = [
+      tds[0].text.strip,
+      tds[1].text.strip,
+      tds[2].attribute("data-#{currency}").text.strip,
+      tds[3].css('a').attribute("data-#{currency}").text.strip,
+      tds[4].text.strip.gsub('*', ''),
+      tds[5].css('a').attribute("data-#{currency}").text.strip,
+      tds[6].text.strip,
+      @ts,
+      currency,
+      ''
+    ]
+    coin[-1] = tr.attribute('id').text
+    h = Hash[@keys.zip(coin)]
+    File.open("#{@path}/#{currency}/#{coin[-1]}.json",'w') { |f| f.write(h.to_json) }
+    File.open("#{@path}/#{coin[-1]}.json",'w') { |f| f.write(h.to_json) } if currency == 'usd'
+    r << h
+  end
+
+  rr = { 'timestamp' => @ts, 'markets' => r }
+  File.open("#{@path}/#{currency}/all.json",'w') {|f| f.write(rr.to_json) }
+  File.open("#{@path}/all.json",'w') {|f| f.write(rr.to_json) } if currency == 'usd'
 end
 
-rr = { 'timestamp' => ts, 'markets' => r }
-File.open("#{path}/all.json",'w') {|f| f.write(rr.to_json) }
+write_to_disk 'usd'
+write_to_disk 'btc'
