94b1ca9255bd8564d891d50b3e603d1471af8d54
Cristian Mircea Messel <mess110@gmail.com>
2017-08-22 20:29:01 +0300

    run saturn script after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,6 +43,7 @@ namespace :deploy do
       execute :touch, release_path.join('tmp/restart.txt')
       # TODO fix this hack
       execute "source /home/kiki/.rvm/environments/ruby-2.1.0@coinmarketcap;cd #{release_path}; rake generate:doc"
+      execute "source /home/kiki/.rvm/environments/ruby-2.1.0@coinmarketcap;cd #{release_path}; ruby saturn.rb"
     end
   end
 
